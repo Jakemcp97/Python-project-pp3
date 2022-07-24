10ed4835bd398144cfba93bc673344303fe9452f
Jakemcp97 <103071250+Jakemcp97@users.noreply.github.com>
2022-07-24 21:06:07 +0100

    first commit, barebones laid out

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Blackjack 
+
+#card class definition 
+
+#create and print card function
+
+#game function 
+
+
+#suits
+suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
+suits_values = {"Spades":"\u2664", "Hearts":"\u2661", "Clubs":"\u2667", "Diamonds":"\u2662"}
+cards = ["A", "2", "3", "4", "5", "6", "7", "8","9", "10", "J", "Q", "k"]
+card_values = {"A":11, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8,"9":9, "10":10, "J":10, "Q":10, "k":10}
